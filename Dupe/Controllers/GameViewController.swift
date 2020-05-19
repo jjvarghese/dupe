c794de7d9845a85f06b05ba683ce6e8c4efec147
Joshua J <jjvarghese@gmail.com>
2020-05-19 20:57:34 +0200

    Configured initial entry point, organised directory

---
+++ Dupe/Controllers/GameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GameViewController.swift
+//  Dupe
+//
+//  Created by Joshua James on 19.05.20.
+//  Copyright © 2020 Cosmic. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
